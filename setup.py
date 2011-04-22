48c969b50a293d9239f987d5f1e49e41c325076c
Marc Brinkmann <git@marcbrinkmann.de>
2011-04-22 11:29:03 +0200

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding=utf8
+
+from distutils.core import setup
+
+setup(name='WTFuniform',
+      version='0.1',
+      description='Use WTForms to output uni-form enabled forms. Extends wtforms and supplies some HTML-generating functions.',
+      author='Marc Brinkmann',
+      author_email='git@marcbrinkmann.de',
+      url='http://github.com/mbr/',
+      packages=['wtfuniform'],
+      install_requires=['wtforms'],
+     )
